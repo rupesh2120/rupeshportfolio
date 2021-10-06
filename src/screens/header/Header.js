4185522d585a2ec81bd30b804c5e822acc02a1ca
Rupesh <rupeshsharma2196@gmail.com>
2021-10-06 21:18:17 +0530

    made changes in header

--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -1,23 +1,29 @@
 import React from "react";
 import "./header.css";
+import person from "../../images/profile.jpeg";
 import Typed from "react-typed";
 
 function Header() {
 	return (
-		<div className="main-info">
-			<h1>Here I am in nutshell</h1>
-			<Typed
-				strings={[
-					" ",
-					"A Software Engineer",
-					"Front-end developer",
-					"React.js developer",
-					"Back-end developer",
-				]}
-				typeSpeed={40}
-				backSpeed={50}
-				loop
-			/>
+		<div className="main-container">
+			<div>
+				<img src={person} alt="person icon" />
+			</div>
+			<div className="main-info">
+				<h1>Here I am in nutshell</h1>
+				<Typed
+					strings={[
+						" ",
+						"A Software Engineer",
+						"Front-end developer",
+						"React.js developer",
+						"Back-end developer",
+					]}
+					typeSpeed={40}
+					backSpeed={50}
+					loop
+				/>
+			</div>
 		</div>
 	);
 }
